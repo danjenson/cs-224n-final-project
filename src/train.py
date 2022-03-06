0ace98b6b33d8aec2f7a000b0aa06452301a741c
Daniel Jenson <daniel.a.jenson@gmail.com>
2022-03-05 22:27:34 -0800

    starting consolidation of training scripts

--- src/train.py
+++ src/train.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 import argparse
+import itertools as it
 import json
 import logging
+import multiprocessing as mp
 import sys
 import yaml
 from types import SimpleNamespace
 
+import torch
 import datasets as hfd
 import transformers as hft
 
@@ -13,6 +16,7 @@ from bashlint.data_tools import (
     bash_parser,
     ast2template,
 )
+from metric_utils import compute_metric
 
 logging.basicConfig(
     filename='gs.log',
@@ -21,17 +25,22 @@ logging.basicConfig(
 )
 
 # TODO:
-# 1. create labels in tokenizer
+# 1. Test tokenization / causal LM
 # 2. compute metric
-# 3. test Causal vs Seq2Seq
-# 4. do we need a data loader?
+# 3. Test GPT-2 vs. BART vs. T-5
+# 4. Try non-templated commands
+# 5. Visualization / errors
 
 
 def train(cfg):
-    '''Train a HuggingFace model.'''
-    tokenizer = hft.AutoTokenizer.from_pretrained(cfg.model.checkpoint)
-    model = hft.AutoModelForSeq2SeqLM.from_pretrained(cfg.model.checkpoint)
-    collator = hft.DataCollatorForSeq2Seq(tokenizer, model)
+    '''Train a HuffingFace model.'''
+    d = resolve(cfg.model.type)
+    ckpt = cfg.model.checkpoint
+    tokenizer = d['tokenizer'].from_pretrained(ckpt)
+    model = d['model'].from_pretrained(cfg.model.checkpoint)
+    collator = d['collator'](tokenizer, model)
+    args = d['args'](**vars(cfg.training))
+    tokenize = d['tokenize']
     trans = cfg.dataset.translate
     ds = hfd.load_from_disk(cfg.dataset.path)
     ds = ds.train_test_split(
@@ -39,13 +48,10 @@ def train(cfg):
         seed=cfg.dataset.splits.seed,
         shuffle=True,
     )
-    ds = ds.map(
-        lambda x: tokenizer(x[trans.source], x[trans.target], truncation=True),
-        batched=True,
-    )
+    ds = ds.map(lambda x: tokenize(tokenizer, x, trans.source, trans.target))
     trainer = hft.Trainer(
         model=model,
-        args=hft.TrainingArguments(**vars(cfg.training)),
+        args=args,
         train_dataset=ds['train'],
         eval_dataset=ds['test'],
         tokenizer=tokenizer,
@@ -53,6 +59,49 @@ def train(cfg):
     )
     trainer.train()
     model.save_pretrained(cfg.model.output_path)
+
+
+def resolve(model_type):
+    '''Resolve task specific classes and functions.'''
+    return {
+        'seq2seq': {
+            'tokenizer': hft.AutoTokenizer,
+            'model': hft.AutoModelForCausalLM,
+            'collator': hft.DataCollatorForSeq2Seq,
+            'args': hft.Seq2SeqTrainingArguments,
+            'tokenize': tokenize_seq2seq,
+        },
+        'causal': {
+            'tokenizer': hft.AutoTokenizer,
+            'model': hft.AutoModelForCausalLM,
+            'collator': hft.DataCollatorForLanguageModeling,
+            'args': hft.TrainingArguments,
+            'tokenize': tokenize_causal,
+        },
+    }[model_type]
+
+
+def tokenize_seq2seq(tokenizer, examples, source, target):
+    '''Tokenize for a seq2seq model.'''
+    inputs = tokenizer(examples[source], truncation=True)
+    with tokenizer.as_target_tokenizer():
+        inputs['labels'] = tokenizer(examples[target],
+                                     truncation=True)['input_ids']
+    return inputs
+
+
+def tokenize_causal(tokenizer, examples, source, target):
+    '''Tokenize for a causal model.'''
+    args = zip(examples, it.repeat((source, target)))
+    with mp.Pool() as p:
+        encoded = p.starmap(encode_causal, args)
+    return tokenizer(encoded, add_special_tokens=True, truncation=True)
+
+
+def encode_causal(example, source, target):
+    '''Encode an example for a causal model.'''
+    s, t = example[source], example[target]
+    return f'<|{source}|> {s} <|{target}|> {t}'
 
 
 def build_templated_dataset():
@@ -78,6 +127,16 @@ def templatize(cmd):
     return cmd
 
 
+def evaluate(cfg):
+    '''Evaluate a model given a config.'''
+    d = resolve(cfg.model.type)
+    tokenizer = d['tokenizer'].from_pretrained(cfg.model.checkpoint)
+    model = d['model'].from_pretrained(cfg.model.output_path)
+    model.to(torch.device('cuda'))
+    # TODO(yingxiao)
+    raise NotImplementedError()
+
+
 def load_config(yaml_path):
     '''Load yaml config as a nested namespace object.'''
     with open(yaml_path) as f:
@@ -100,7 +159,7 @@ def parse_args(argv):
     )
     parser.add_argument(
         '-t',
-        '--train_yaml',
+        '--train',
         help='train a model with given yaml config',
         default='bart.yaml',
     )
@@ -108,6 +167,12 @@ def parse_args(argv):
         '-tds',
         help='build a templated command Dataset',
         action='store_true',
+    )
+    parser.add_argument(
+        '-e',
+        '--evaluate',
+        help='evaluate a model with a given yaml config',
+        default='bart.yaml',
     )
     return parser.parse_args(argv[1:])
 
@@ -118,6 +183,9 @@ if __name__ == '__main__':
         tds = build_templated_dataset()
         tds.save_to_disk('tds')
         logging.info('saved templated dataset to "tds"')
-    if args.train_yaml:
-        cfg = load_config(args.train_yaml)
+    if args.train:
+        cfg = load_config(args.train)
         train(cfg)
+    if args.evaluate:
+        cfg = load_config(args.evaluate)
+        evaluate(cfg)
