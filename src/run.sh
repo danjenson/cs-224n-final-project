a9f49613f61b7bfa13348584869d55258c55d053
Daniel Jenson <daniel.a.jenson@gmail.com>
2022-03-06 22:43:58 -0800

    updating run

--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-./model.py dataset
-./model.py train -c "$1.yaml"
-./model.py predict -c "$1.yaml"
-./model.py score -c "$1.yaml"
+./model.py train -c "$1.yaml" \
+  && ./model.py predict -c "$1.yaml" \
+  && ./model.py score -c "$1.yaml"
