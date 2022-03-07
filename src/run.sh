f0606119c56a112bd74c390c19fa1b6688c4afbc
Daniel Jenson <daniel.a.jenson@gmail.com>
2022-03-06 22:38:20 -0800

    tweaking run file

--- src/run.sh
+++ src/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-NAME=$1
 ./model.py dataset
-./model.py train -c $NAME.yaml
-./model.py predict -c $NAME.yaml
-./model.py score -c $NAME.yaml
+./model.py train -c "$1.yaml"
+./model.py predict -c "$1.yaml"
+./model.py score -c "$1.yaml"
